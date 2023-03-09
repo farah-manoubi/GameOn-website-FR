242e3d5dbf78c5009bebc3d9d99b67a4cfead59c
farah <farah.manoubi@hotmail.com>
2023-03-09 13:43:58 +0100

    formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,9 +31,7 @@ function closeModal() {
 
 // Soumission du formulaire
 let form = document.getElementById("reserve");
-form.addEventListener('submit', validate);
-
-var formValid = document.getElementById("boutonEnvoi");
+form.addEventListener("submit", validate);
 
 // Fonction qui permet de vérifier si le formulaire contient des erreurs
 function validate(event){
@@ -53,6 +51,9 @@ function validate(event){
     document.getElementsByClassName("formData")[0].getAttribute("data-error");
     document.getElementsByClassName("formData")[0].setAttribute("data-error-visible", "true");
     validityForm = false;
+  }else{
+    document.getElementsByClassName("formData")[0].getAttribute("data-error");
+    document.getElementsByClassName("formData")[0].setAttribute("data-error-visible", "");
   }
   
   // Vérifie si le nom est vide ou s'il est inférieur à 2 caractères
@@ -60,6 +61,9 @@ function validate(event){
     document.getElementsByClassName("formData")[1].getAttribute("data-error");
     document.getElementsByClassName("formData")[1].setAttribute("data-error-visible", "true"); 
     validityForm = false;
+  }else{
+    document.getElementsByClassName("formData")[1].getAttribute("data-error");
+    document.getElementsByClassName("formData")[1].setAttribute("data-error-visible", "");
   }
 
   // Vérifie si l'adresse mail est vide ou si sa structure est respectée
@@ -67,6 +71,9 @@ function validate(event){
     document.getElementsByClassName("formData")[2].getAttribute("data-error");
     document.getElementsByClassName("formData")[2].setAttribute("data-error-visible", "true");
     validityForm = false;
+  }else{
+    document.getElementsByClassName("formData")[2].getAttribute("data-error");
+    document.getElementsByClassName("formData")[2].setAttribute("data-error-visible", "");
   }
   
   // Vérifie si le champs est vide
@@ -74,6 +81,9 @@ function validate(event){
     document.getElementsByClassName("formData")[4].getAttribute("data-error");
     document.getElementsByClassName("formData")[4].setAttribute("data-error-visible", "true");
     validityForm = false;
+  }else{
+    document.getElementsByClassName("formData")[4].getAttribute("data-error");
+    document.getElementsByClassName("formData")[4].setAttribute("data-error-visible", "");
   }
    
   // Vérifie si une location a été sélectionnée
@@ -81,21 +91,27 @@ function validate(event){
     document.getElementsByClassName("formData")[5].getAttribute("data-error");
     document.getElementsByClassName("formData")[5].setAttribute("data-error-visible", "true");
     validityForm = false;  
-  }   
+  }else{
+    document.getElementsByClassName("formData")[5].getAttribute("data-error");
+    document.getElementsByClassName("formData")[5].setAttribute("data-error-visible", "");
+  }
   
   // Vérifie si les conditions d'utilisation ont été acceptées
   if(acceptCondition == false){
     document.getElementsByClassName("formData")[6].getAttribute("data-error");
     document.getElementsByClassName("formData")[6].setAttribute("data-error-visible", "true");
     validityForm = false;
+  }else{
+    document.getElementsByClassName("formData")[6].getAttribute("data-error");
+    document.getElementsByClassName("formData")[6].setAttribute("data-error-visible", "");
   }
 
   // Le formulaire n'est pas envoyé s'il y a une erreur
   if(validityForm == false){
     event.preventDefault();
-    alert('Le formulaire est incorrect');
   }
 
 }
 
-formValid.addEventListener('click', validate);
+
+
